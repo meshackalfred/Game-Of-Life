71a274f93fa8c764a4eec68ee89e031be184d3db
Meshack Alfred <meshackalfred@gmail.com>
2020-10-08 00:42:29 +0200

    final stage

--- Data/Environment.cs
+++ Data/Environment.cs
@@ -32,6 +32,39 @@
                 return this.cell[i,j].is_alive();
                 return false;
         }
-    }
+        public int aliveNeighbors(int i,int j) {
+                int c=0;
+                c += this.is_alive(i-1,j-1)?1:0;
+                c += this.is_alive(i-1,j)?1:0;
+                c += this.is_alive(i-1,j+1)?1:0;
+                c += this.is_alive(i,j-1)?1:0;
+                c += this.is_alive(i,j+1)?1:0;
+                c += this.is_alive(i+1,j-1)?1:0;
+                c += this.is_alive(i+1,j)?1:0;
+                c += this.is_alive(i+1,j+1)?1:0;
+                return c;   
+         }
+         public void nextConwayStep() {
+                    int n;
+                    bool[,] aux = new bool[this.rows,this.cols];
+                    for(var i=0; i<this.rows; i++)
+                    for(var j=0; j<this.cols; j++) {
+                    n = this.aliveNeighbors(i,j);
+                    if(n==3) //Conway’s original rule
+                    aux[i,j] = true;
+                    else if (n==2 && this.is_alive(i,j))
+                    aux[i,j] = true;
+                    else
+                    aux[i,j] = false;
+                    }
+                    for(var i=0; i<this.rows; i++)
+                    for(var j=0; j<this.cols; j++) {
+                    if(aux[i,j])
+                    this.live(i,j);
+                    else
+                    this.die(i,j);
+                    }
+           }
+}
 }
     
